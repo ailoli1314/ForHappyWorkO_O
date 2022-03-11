4a866d16403dd982a7f5623d22bb934d42174af4
ailoli1314 <209346818@qq.com>
2022-03-11 19:47:22 +0800

    qwe

--- guib_annotation/src/main/java/com/example/guib_annotation/Bind.java
+++ guib_annotation/src/main/java/com/example/guib_annotation/Bind.java
@@ -6,9 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * @author Created by qiyei2015 on 2018/4/14.
  * @version: 1.0
- * @email: 1273482124@qq.com
  * @description: Bind注解 指明注解作用域为类
  */
 @Target({ElementType.TYPE})
